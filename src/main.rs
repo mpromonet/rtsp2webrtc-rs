90384bce31406ae92ed78223a89fddeeb50cc29a
Michel Promonet <michel.promonet@free.fr>
2024-06-09 15:17:21 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** SPDX-License-Identifier: Unlicense
+**
+** -------------------------------------------------------------------------*/
+
+use actix_files::Files;
+use actix_web::{get, post, web, App, HttpServer, HttpRequest, HttpResponse};
+use clap::Parser;
+use log::info;
+use std::sync::Arc;
+use serde_json::json;
+use tokio::sync::broadcast;
+use webrtc::{
+    api::{interceptor_registry::register_default_interceptors, APIBuilder, API},
+    ice_transport::{ice_connection_state::RTCIceConnectionState, ice_server::RTCIceServer},
+    interceptor::registry::Registry,
+    media::Sample,
+    peer_connection::{
+        configuration::RTCConfiguration, peer_connection_state::RTCPeerConnectionState,
+        sdp::session_description::RTCSessionDescription,
+    },
+    rtp_transceiver::rtp_codec::RTCRtpCodecCapability,
+    track::track_local::{track_local_static_sample::TrackLocalStaticSample, TrackLocal},
+};
+
+mod rtspclient;
+
+#[derive(Parser)]
+struct Opts {
+    #[arg(long)]
+    url: url::Url,
+
+    #[clap(short)]
+    transport: Option<String>,    
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let opts = Opts::parse();
+
+    let (tx, rx) = broadcast::channel::<Vec<u8>>(100);
+
+    // start the RTSP clients
+    tokio::spawn(rtspclient::run(opts.url.clone(), opts.transport.clone(), tx.clone()));
+
+    // webrtc
+    let mut m = webrtc::api::media_engine::MediaEngine::default();
+    m.register_default_codecs().unwrap();
+    let mut registry = Registry::new();
+    registry = register_default_interceptors(registry, &mut m).unwrap();
+
+    // Create the API object with the MediaEngine
+    let api = Arc::new(APIBuilder::new()
+        .with_media_engine(m)
+        .with_interceptor_registry(registry)
+        .build());
+
+    info!("start actix web server");
+    HttpServer::new( move || {
+        App::new().app_data(web::Data::new(api.clone()))
+            .service(version)
+            .service(whep)
+            .service(web::redirect("/", "/index.html"))
+            .service(Files::new("/", "./www"))
+    })
+    .bind(("0.0.0.0", 8080)).unwrap()
+    .run()
+    .await
+    .unwrap();
+
+
+    info!("Done");
+}
+
+#[get("/api/version")]
+async fn version() -> HttpResponse {
+    let data = json!("version");
+
+    HttpResponse::Ok().json(data)
+}
+
+#[post("/api/whep")]
+async fn whep(bytes: web::Bytes, data: web::Data<Arc<API>>) -> HttpResponse {
+
+    let api = data.get_ref();
+
+    let downstream_cfg = RTCConfiguration {
+        ice_servers: vec![RTCIceServer {
+            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    let downstream_conn = Arc::new(api.new_peer_connection(downstream_cfg).await.unwrap());
+    let track = Arc::new(TrackLocalStaticSample::new(
+        RTCRtpCodecCapability {
+            mime_type: "video/h264".to_owned(),
+            ..Default::default()
+        },
+        format!("video"),
+        "video".to_owned(),
+    ));    
+    let sender = downstream_conn
+        .add_track(Arc::clone(&track) as Arc<dyn TrackLocal + Send + Sync>)
+        .await.unwrap();
+
+    let sdp_string = String::from_utf8(bytes.to_vec()).unwrap();
+    let sdp: RTCSessionDescription = serde_json::from_str(&sdp_string).unwrap();
+    downstream_conn.set_remote_description(sdp).await.unwrap();
+    let answer = downstream_conn.create_answer(None).await.unwrap();
+    downstream_conn.set_local_description(answer.clone()).await.unwrap();
+
+    HttpResponse::Ok().body(answer.sdp.clone())
+}
---
+++ src/rtspclient.rs
@@ -0,0 +1,147 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** SPDX-License-Identifier: Unlicense
+**
+** -------------------------------------------------------------------------*/
+
+use retina::client::{SessionGroup, SetupOptions, Transport};
+use retina::codec::{CodecItem, VideoFrame, VideoParameters};
+use anyhow::{anyhow, Error};
+use log::{debug, error, info};
+use std::sync::Arc;
+use std::vec;
+use tokio::sync::broadcast;
+use futures::StreamExt;
+use std::io::Cursor;
+use std::io::prelude::*;
+
+pub async fn run(url: url::Url, transport: Option<String>, tx: broadcast::Sender<Vec<u8>>) -> Result<(), Error> {
+    let session_group = Arc::new(SessionGroup::default());
+    let r = run_inner(url, transport, session_group.clone(), tx).await;
+    if let Err(e) = session_group.await_teardown().await {
+        error!("TEARDOWN failed: {}", e);
+    }
+    r
+}
+
+const MARKER: [u8; 4] = [0, 0, 0, 1];
+
+pub fn avcc_to_annex_b(
+    data: &[u8]
+) -> Result<Vec<u8>, Error> {
+    let mut nal_units = vec![];
+    let mut data_cursor = Cursor::new(data);
+    let mut nal_lenght_bytes = [0u8; 4];
+    while let Ok(_) = data_cursor.read_exact(&mut nal_lenght_bytes) {
+        let nal_length = u32::from_be_bytes(nal_lenght_bytes) as usize;
+
+        if nal_length == 0 {
+            return Err(anyhow!("NalLenghtParseError"));
+        }
+        let mut nal_unit = vec![0u8; nal_length];
+        data_cursor.read_exact(&mut nal_unit)?;
+
+        nal_units.extend_from_slice(&MARKER);
+        nal_units.extend_from_slice(&nal_unit);
+    }
+    Ok(nal_units)
+}
+
+fn decode_cfg(data: &[u8]) -> Result<Vec<u8>, Error> {
+    let sps_len = u16::from_be_bytes([data[6], data[7]]) as usize;
+    let pps_len = u16::from_be_bytes([data[8 + sps_len + 1], data[9 + sps_len + 1]]) as usize;
+    if ((8+sps_len) > data.len()) || ((10+sps_len+1+pps_len) > data.len()) {
+        return Err(anyhow!("Error decoding cfg"));
+    }
+    let mut cfg: Vec<u8> = vec![];
+    cfg.extend_from_slice(&MARKER);
+    cfg.extend_from_slice(&data[8..8+sps_len]);
+    cfg.extend_from_slice(&MARKER);
+    cfg.extend_from_slice(&data[10+sps_len+1..10+sps_len+1+pps_len]);
+    Ok(cfg)
+}
+
+fn process_video_frame(m: VideoFrame, video_params: VideoParameters, tx: broadcast::Sender<Vec<u8>>) {
+    debug!(
+        "{}: size:{} is_random_access_point:{} has_new_parameters:{}",
+        m.timestamp().timestamp(),
+        m.data().len(),
+        m.is_random_access_point(),
+        m.has_new_parameters(),
+    );
+
+    let mut data: Vec<u8> = vec![];
+    if m.is_random_access_point() {        
+        let extra_data = video_params.extra_data();
+        debug!("extra_data:{:?}", extra_data);
+    
+        let cfg = decode_cfg(extra_data).unwrap();
+        debug!("CFG: {:?}", cfg);    
+        data.extend_from_slice(cfg.as_slice());
+    }
+    let nal_units = avcc_to_annex_b(m.data()).unwrap();
+    data.extend_from_slice(nal_units.as_slice());
+
+    if let Err(e) = tx.send(data) {
+        error!("Error broadcasting message: {}", e);
+    }                        
+}
+
+async fn run_inner(url: url::Url, transport: Option<String>, session_group: Arc<SessionGroup>, tx: broadcast::Sender<Vec<u8>>) -> Result<(), Error> {
+    let stop = tokio::signal::ctrl_c();
+
+    let mut session = retina::client::Session::describe(
+        url.clone(),
+        retina::client::SessionOptions::default()
+            .session_group(session_group),
+    )
+    .await?;
+    debug!("{:?}", session.streams());
+
+    let video_stream = session
+        .streams()
+        .iter()
+        .position(|s| s.media() == "video" && s.encoding_name() == "h264")
+        .ok_or_else(|| anyhow!("couldn't find video stream"))?;
+
+    let transport_value = match transport {
+        Some(t) => t.parse::<Transport>().unwrap(),
+        None => Transport::default(), 
+    };    
+    let options = SetupOptions::transport(SetupOptions::default(), transport_value);
+    session
+        .setup(video_stream, options)
+        .await?;
+
+    let video_params = match session.streams()[video_stream].parameters() {
+        Some(retina::codec::ParametersRef::Video(v)) => v.clone(),
+        Some(_) => unreachable!(),
+        None => unreachable!(),
+    };
+    info!("video_params:{:?}", video_params);
+
+    let mut videosession = session
+        .play(retina::client::PlayOptions::default())
+        .await?
+        .demuxed()?;
+
+    
+    tokio::pin!(stop);
+    loop {
+        tokio::select! {
+            item = videosession.next() => {
+                match item.ok_or_else(|| anyhow!("EOF"))?? {
+                    CodecItem::VideoFrame(m) => process_video_frame(m, video_params.clone(), tx.clone()),
+                    _ => continue,
+                };
+            },
+            _ = &mut stop => {
+                break;
+            },
+        }
+    }
+    Ok(())
+}
